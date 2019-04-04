bd460c13c47dc04e2264a2cc802847edc12492a0
Kyle McKee <mckee.kyle@gmail.com>
2019-04-04 10:22:06 -0400

    Added react router dom and stripped boilerplate

---
+++ src/StarWarsApp.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./StarWarsApp.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">App goes here.</div>;
+  }
+}
+
+export default App;
